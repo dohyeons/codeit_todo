3ae1f3a91c9af6db3aa6b8a4c4c14ab1f71488f1
dohyeons <ddhhss0603@gmail.com>
2024-12-29 02:03:36 +0900

    ✨ feat:List 컴포넌트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
-import Image from "next/image";
-import plus from "../../public/image/plus.svg";
 import List from "@/components/List";
+import getItems from "@/api/getItems";
 
-export default function Home() {
+export default async function Home() {
+	const initialTodos = await getItems();
+
 	return (
 		<div className="container-main">
-			<div className="flex gap-[8px] tablet:gap-[16px] h-input-btn mb-[24px] tablet:mb-[40px]">
-				<input
-					className="w-full max-w-[1016px] rounded bg-primary-100 placeholder:text-primary-500 "
-					placeholder="할 일을 입력해주세요"
-				/>
-				<button className="min-w-[56px] flex items-center justify-center tablet:min-w-[168px] gap-[4px] rounded bg-primary-200">
-					<Image alt="plus" src={plus} />
-					<span className="hidden tablet:block">추가하기</span>
-				</button>
-			</div>
-			<List />
+			<List initialTodos={initialTodos} />
 		</div>
 	);
 }
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -3,54 +3,45 @@
 import Image from "next/image";
 import todo from "../../../public/image/todo.svg";
 import done from "../../../public/image/done.svg";
-import getItems from "@/api/getItems";
-import { useState, useEffect } from "react";
 import TodoList from "@/components/List/TodoList";
+import useTodos from "@/hooks/useTodos";
+import InputForm from "@/components/InputForm";
 
-export default function List() {
-	const [todos, setTodos] = useState<
-		{ isCompleted: boolean; name: string; id: number }[]
-	>([]);
-	const [isLoading, setIsLoading] = useState(true);
+export default function List({
+	initialTodos,
+}: {
+	initialTodos: { id: number; isCompleted: boolean; name: string }[];
+}) {
+	const { todos, isLoading, fetchTodos } = useTodos(initialTodos);
 
-	const fetchTodos = async () => {
-		setIsLoading(true);
-		const res = await getItems();
-		setTodos(res);
-		setIsLoading(false);
-	};
-
-	useEffect(() => {
-		fetchTodos();
-	}, []);
-	function refetchTodos() {
-		fetchTodos();
-	}
 	const incompleteTodos = todos.filter(todo => !todo.isCompleted);
 	const completedTodos = todos.filter(todo => todo.isCompleted);
 
 	return (
-		<div className="grid grid-cols-1 gap-[48px] desktop:grid-cols-2 desktop:gap-[24px]">
-			<section>
-				<Image src={todo} alt={"todo"} />
-				{isLoading ? (
-					<div className="w-full mt-[16px]">
-						<span>데이터를 불러오고 있습니다...!</span>
-					</div>
-				) : (
-					<TodoList todos={incompleteTodos} refetchTodos={refetchTodos} />
-				)}
-			</section>
-			<section>
-				<Image src={done} alt={"done"} />
-				{isLoading ? (
-					<div className="w-full mt-[16px]">
-						<span>데이터를 불러오고 있습니다...!</span>
-					</div>
-				) : (
-					<TodoList todos={completedTodos} refetchTodos={refetchTodos} />
-				)}
-			</section>
-		</div>
+		<>
+			<InputForm onAdd={fetchTodos} />
+			<div className="grid grid-cols-1 gap-[48px] desktop:grid-cols-2 desktop:gap-[24px]">
+				<section>
+					<Image src={todo} alt={"todo"} />
+					{isLoading ? (
+						<div className="w-full mt-[16px]">
+							<span>데이터를 불러오고 있습니다...!</span>
+						</div>
+					) : (
+						<TodoList todos={incompleteTodos} refetchTodos={fetchTodos} />
+					)}
+				</section>
+				<section>
+					<Image src={done} alt={"done"} />
+					{isLoading ? (
+						<div className="w-full mt-[16px]">
+							<span>데이터를 불러오고 있습니다...!</span>
+						</div>
+					) : (
+						<TodoList todos={completedTodos} refetchTodos={fetchTodos} />
+					)}
+				</section>
+			</div>
+		</>
 	);
 }
