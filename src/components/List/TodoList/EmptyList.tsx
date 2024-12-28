16771d37ac702bac35a20b4986ea26cf184a7da1
dohyeons <ddhhss0603@gmail.com>
2024-12-29 00:52:10 +0900

    ✨ feat: todo가 없을 때 보여줄 EmptyList 컴포넌트 구현

---
+++ src/components/List/TodoList/EmptyList.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import todo_large from "../../../../public/image/todo_large.svg";
+
+export default function EmptyList() {
+	return (
+		<div className="flex flex-col justify-center items-center tablet:mt-[24px] desktop:mt-[64px]">
+			<Image
+				src={todo_large}
+				alt="todo_large"
+				className="size-[120px] tablet:size-[240px]"
+			/>
+			<span className="whitespace-pre-line text-center">
+				할 일이 없어요.{"\n"}TODO를 새롭게 추가해주세요!
+			</span>
+		</div>
+	);
+}
