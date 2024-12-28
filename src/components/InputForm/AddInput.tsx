56c1dc925f74c1328482a4285c2e7b12d6a68d69
dohyeons <ddhhss0603@gmail.com>
2024-12-29 02:02:16 +0900

    ✨ feat: 할 일 입력 인풋 분리

---
+++ src/components/InputForm/AddInput.tsx
@@ -0,0 +1,19 @@
+export default function AddInput({
+	value,
+	onChange,
+	onKeyDown,
+}: {
+	value: string;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}) {
+	return (
+		<input
+			className="w-full max-w-[1016px] rounded bg-primary-100 placeholder:text-primary-500 "
+			placeholder="할 일을 입력해주세요"
+			value={value}
+			onChange={onChange}
+			onKeyDown={onKeyDown}
+		/>
+	);
+}
