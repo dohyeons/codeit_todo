2992b2439b8b96322e5936c62f2671bc52ef148a
dohyeons <ddhhss0603@gmail.com>
2024-12-28 14:08:12 +0900

    💄 design: 메인 페이지 반응형 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <div>안녕 여기는 홈페이지</div>;
+	return (
+		<div className="grid grid-cols-1 gap-[48px] desktop:grid-cols-2 desktop:gap-[24px]">
+			<section>첫번째 콘텐츠</section>
+			<section>두번째 콘텐츠</section>
+		</div>
+	);
 }
