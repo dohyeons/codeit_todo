4af62cba1b8693a6e02f2d0f2e5919316b45ce4e
dohyeons <ddhhss0603@gmail.com>
2024-12-29 23:52:27 +0900

    ✨ feat: 메모 퍼블리싱

--- src/components/TodoDetail/index.tsx
+++ src/components/TodoDetail/index.tsx
@@ -9,6 +9,7 @@ import img from "../../../public/image/img.svg";
 import plus_large from "../../../public/image/plus_large.svg";
 import check from "../../../public/image/check.svg";
 import x from "../../../public/image/x.svg";
+import memo_heading from "../../../public/image/memo_heading.svg";
 
 export default function TodoDetail({
 	initialTodoDetail,
@@ -53,9 +54,10 @@ export default function TodoDetail({
 					</div>
 				</article>
 				<article
-					className={`border border-red-300 desktop:max-x-[588px] w-full h-[311px] rounded  bg-[url('/image/memo.svg')]`}
+					className={`border border-red-300 desktop:max-x-[588px] w-full h-[311px] rounded  bg-[url('/image/memo.svg')] flex flex-col py-[24px] px-[16px] items-center gap-[16px]`}
 				>
-					메모
+					<Image src={memo_heading} alt="memo_heading" className="mb-[16px]" />
+					<textarea className="w-full h-[311px] outline-none bg-transparent overflow-y:auto scrollbar-thin resize-none scrollbar-thin scrollbar-thumb-amber-200" />
 				</article>
 			</div>
 			<div className="flex gap-[7px] tablet:gap-[16px] desktop:justify-end justify-center mt-[24px]">
