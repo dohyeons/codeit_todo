2d692d6ee381542fdcf206e463b93b98efb38e9c
dohyeons <ddhhss0603@gmail.com>
2024-12-30 21:19:28 +0900

    ✨ feat: 기능만 구현

--- src/components/TodoDetail/index.tsx
+++ src/components/TodoDetail/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import getTodoDetail from "@/api/getTodoDetail";
 import { TodoListDetailType } from "@/types";
-import Image from "next/image";
+import NextImage from "next/image";
 import { useEffect, useState } from "react";
 import img from "../../../public/image/img.svg";
 import plus_large from "../../../public/image/plus_large.svg";
@@ -13,6 +12,9 @@ import DetailHeader from "@/components/TodoDetail/DetailHeader";
 import edit from "../../../public/image/edit.svg";
 import deleteTodo from "@/api/deleteTodo";
 import { useRouter } from "next/navigation";
+import updateDetail from "@/api/updateDetail";
+import FileResizer from "react-image-file-resizer";
+
 export default function TodoDetail({
 	initialTodoDetail: { tenantId, ...res },
 }: {
@@ -20,6 +22,8 @@ export default function TodoDetail({
 }) {
 	const [todoDetail, setTodoDetail] = useState({
 		...res,
+		memo: res.memo || "",
+		imageUrl: res.imageUrl || "",
 	});
 	const [isTodoDetailChanging, setIsTodoDetailChanging] = useState(false);
 	const router = useRouter();
@@ -27,8 +31,8 @@ export default function TodoDetail({
 	useEffect(() => {
 		function isChanged<T extends object>(obj1: T, obj2: T): boolean {
 			return (Object.keys(obj1) as (keyof T)[]).some(key => {
-				if (obj2[key] === "") {
-					return obj1[key] !== null;
+				if (obj1[key] === null) {
+					return obj2[key] !== "";
 				}
 				return obj1[key] !== obj2[key];
 			});
@@ -36,19 +40,11 @@ export default function TodoDetail({
 		setIsTodoDetailChanging(isChanged(res, todoDetail));
 	}, [res, todoDetail]);
 
-	async function refetchTodoDetail() {
-		const { tenantId, ...res } = await getTodoDetail(todoDetail.id);
-		setTodoDetail({
-			...res,
-			imageUrl: res.imageUrl || "",
-			memo: res.memo || "",
-		});
-	}
-
 	function fileInputChange(e: React.ChangeEvent<HTMLInputElement>) {
 		const file = e.target.files?.[0];
 		if (!file) return;
-		// 일단 file이 0이 아니라면 걍 return.
+
+		// 파일 검증
 		if (e.target.files && e.target.files.length > 1) {
 			return;
 		}
@@ -58,19 +54,22 @@ export default function TodoDetail({
 		if (file.size > 5 * 1024 * 1024) {
 			return;
 		}
-
 		if (!/^[a-zA-Z0-9_.-]+$/.test(file.name)) {
-			console.log(file.name);
 			return;
 		}
 
-		const reader = new FileReader();
-		reader.onload = data => {
-			if (typeof data.target?.result === "string") {
-				setTodoDetail({ ...todoDetail, imageUrl: data.target?.result });
-			}
-		};
-		reader.readAsDataURL(file);
+		FileResizer.imageFileResizer(
+			file,
+			800,
+			800,
+			"JPEG",
+			60,
+			0,
+			uri => {
+				setTodoDetail({ ...todoDetail, imageUrl: uri as string });
+			},
+			"base64"
+		);
 	}
 
 	function handleMemoChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -84,11 +83,19 @@ export default function TodoDetail({
 		await deleteTodo(todoDetail.id);
 		router.push("/");
 	}
+	async function onClickCompleteButton() {
+		await updateDetail(todoDetail.id, todoDetail);
+		router.push("/");
+	}
+	function changeCompleteStatus() {
+		setTodoDetail({ ...todoDetail, isCompleted: !todoDetail.isCompleted });
+	}
+	console.log(todoDetail);
 	return (
 		<>
 			<DetailHeader
 				todoDetail={todoDetail}
-				refetchTodoDetail={refetchTodoDetail}
+				changeCompleteStatus={changeCompleteStatus}
 				handleMemoNameChange={handleMemoNameChange}
 			/>
 			<div className="flex flex-col desktop:flex-row gap-[15px] tablet:gap-[24px] ">
@@ -98,9 +105,11 @@ export default function TodoDetail({
 							? `url(${todoDetail.imageUrl})`
 							: "none",
 					}}
-					className={`flex bg-center bg-cover  items-center justify-center relative border-2 border-dashed border-primary-300 w-full desktop:max-w-[384px] h-[311px] bg-slate-50 rounded`}
+					className={`flex bg-center bg-cover ${
+						!todoDetail.imageUrl && "border-2 border-dashed border-primary-300"
+					}  items-center justify-center relative  w-full desktop:max-w-[384px] h-[311px] bg-slate-50 rounded`}
 				>
-					{!todoDetail.imageUrl && <Image src={img} alt="img" />}
+					{!todoDetail.imageUrl && <NextImage src={img} alt="img" />}
 					<label htmlFor="file-input">
 						<div
 							className={`rounded-full cursor-pointer bottom-[16px] right-[16px] ${
@@ -109,7 +118,10 @@ export default function TodoDetail({
 									: "bg-primary-200"
 							} absolute size-[64px] flex justify-center items-center`}
 						>
-							<Image src={todoDetail.imageUrl ? edit : plus_large} alt="edit" />
+							<NextImage
+								src={todoDetail.imageUrl ? edit : plus_large}
+								alt="edit"
+							/>
 						</div>
 					</label>
 					<input
@@ -123,7 +135,11 @@ export default function TodoDetail({
 				<article
 					className={`desktop:max-x-[588px] w-full h-[311px] rounded  bg-[url('/image/memo.svg')] flex flex-col py-[24px] px-[16px] items-center gap-[16px]`}
 				>
-					<Image src={memo_heading} alt="memo_heading" className="mb-[16px]" />
+					<NextImage
+						src={memo_heading}
+						alt="memo_heading"
+						className="mb-[16px]"
+					/>
 					<textarea
 						value={todoDetail.memo || ""}
 						onChange={handleMemoChange}
@@ -133,19 +149,20 @@ export default function TodoDetail({
 			</div>
 			<div className="flex gap-[7px] tablet:gap-[16px] desktop:justify-end justify-center mt-[24px]">
 				<button
+					onClick={onClickCompleteButton}
 					disabled={!isTodoDetailChanging}
 					className={`w-[168px] h-[56px] flex items-center ${
 						isTodoDetailChanging ? "bg-btn-complete" : "bg-primary-200"
 					} justify-center  gap-[4px] rounded `}
 				>
-					<Image alt="plus" src={check} />
+					<NextImage alt="plus" src={check} />
 					<span>수정 완료</span>
 				</button>
 				<button
 					onClick={onClickDeleteButton}
 					className="w-[168px] text-small-semi text-white h-[56px] flex items-center justify-center  gap-[4px] rounded bg-btn-delete"
 				>
-					<Image alt="plus" src={x} />
+					<NextImage alt="plus" src={x} />
 					<span>삭제하기</span>
 				</button>
 			</div>
