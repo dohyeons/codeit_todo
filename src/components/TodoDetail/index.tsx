780444a73f7a1b012c58e0170366705ea4e0c030
dohyeons <ddhhss0603@gmail.com>
2024-12-30 15:47:09 +0900

    ✨ feat: 값 변경 시 수정 완료 버튼 배경색 변환 및 삭제 구현

--- src/components/TodoDetail/index.tsx
+++ src/components/TodoDetail/index.tsx
@@ -3,7 +3,7 @@
 import getTodoDetail from "@/api/getTodoDetail";
 import { TodoListDetailType } from "@/types";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import img from "../../../public/image/img.svg";
 import plus_large from "../../../public/image/plus_large.svg";
 import check from "../../../public/image/check.svg";
@@ -11,24 +11,38 @@ import x from "../../../public/image/x.svg";
 import memo_heading from "../../../public/image/memo_heading.svg";
 import DetailHeader from "@/components/TodoDetail/DetailHeader";
 import edit from "../../../public/image/edit.svg";
-
+import deleteTodo from "@/api/deleteTodo";
+import { useRouter } from "next/navigation";
 export default function TodoDetail({
-	initialTodoDetail,
+	initialTodoDetail: { tenantId, ...res },
 }: {
 	initialTodoDetail: TodoListDetailType;
 }) {
-	const [todoDetail, setTodoDetail] = useState(initialTodoDetail);
-	const [memoImage, setMemoImage] = useState<string>(
-		initialTodoDetail.imageUrl || ""
-	);
-	const [memoValue, setMemoValue] = useState(initialTodoDetail.memo || "");
+	const [todoDetail, setTodoDetail] = useState({
+		...res,
+	});
+	const [isTodoDetailChanging, setIsTodoDetailChanging] = useState(false);
+	const router = useRouter();
 
-	const [memoName, setMemoName] = useState(initialTodoDetail.name);
-	const [isMemoNameChanging, setIsMemoNameChanging] = useState(false);
+	useEffect(() => {
+		function isChanged<T extends object>(obj1: T, obj2: T): boolean {
+			return (Object.keys(obj1) as (keyof T)[]).some(key => {
+				if (obj2[key] === "") {
+					return obj1[key] !== null;
+				}
+				return obj1[key] !== obj2[key];
+			});
+		}
+		setIsTodoDetailChanging(isChanged(res, todoDetail));
+	}, [res, todoDetail]);
 
 	async function refetchTodoDetail() {
-		const res = await getTodoDetail(todoDetail.id);
-		setTodoDetail(res);
+		const { tenantId, ...res } = await getTodoDetail(todoDetail.id);
+		setTodoDetail({
+			...res,
+			imageUrl: res.imageUrl || "",
+			memo: res.memo || "",
+		});
 	}
 
 	function fileInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -53,23 +67,22 @@ export default function TodoDetail({
 		const reader = new FileReader();
 		reader.onload = data => {
 			if (typeof data.target?.result === "string") {
-				console.log(data.target?.result);
-				setMemoImage(data.target?.result);
+				setTodoDetail({ ...todoDetail, imageUrl: data.target?.result });
 			}
 		};
 		reader.readAsDataURL(file);
 	}
 
 	function handleMemoChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-		setMemoValue(e.target.value);
+		setTodoDetail({ ...todoDetail, memo: e.target.value });
 	}
 
 	function handleMemoNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-		setMemoName(e.target.value);
+		setTodoDetail({ ...todoDetail, name: e.target.value });
 	}
-
-	function handleMemoNameClick() {
-		setIsMemoNameChanging(prev => !prev);
+	async function onClickDeleteButton() {
+		await deleteTodo(todoDetail.id);
+		router.push("/");
 	}
 	return (
 		<>
@@ -77,26 +90,26 @@ export default function TodoDetail({
 				todoDetail={todoDetail}
 				refetchTodoDetail={refetchTodoDetail}
 				handleMemoNameChange={handleMemoNameChange}
-				onMemoNameClick={handleMemoNameClick}
-				isMemoNameChanging={isMemoNameChanging}
 			/>
 			<div className="flex flex-col desktop:flex-row gap-[15px] tablet:gap-[24px] ">
 				<article
 					style={{
-						backgroundImage: memoImage ? `url(${memoImage})` : "none",
+						backgroundImage: todoDetail.imageUrl
+							? `url(${todoDetail.imageUrl})`
+							: "none",
 					}}
 					className={`flex bg-center bg-cover  items-center justify-center relative border-2 border-dashed border-primary-300 w-full desktop:max-w-[384px] h-[311px] bg-slate-50 rounded`}
 				>
-					{!memoImage && <Image src={img} alt="img" />}
+					{!todoDetail.imageUrl && <Image src={img} alt="img" />}
 					<label htmlFor="file-input">
 						<div
 							className={`rounded-full cursor-pointer bottom-[16px] right-[16px] ${
-								memoImage
+								todoDetail.imageUrl
 									? "bg-btn-edit border-2 border-primary-900"
 									: "bg-primary-200"
 							} absolute size-[64px] flex justify-center items-center`}
 						>
-							<Image src={memoImage ? edit : plus_large} alt="edit" />
+							<Image src={todoDetail.imageUrl ? edit : plus_large} alt="edit" />
 						</div>
 					</label>
 					<input
@@ -112,18 +125,26 @@ export default function TodoDetail({
 				>
 					<Image src={memo_heading} alt="memo_heading" className="mb-[16px]" />
 					<textarea
-						value={memoValue}
+						value={todoDetail.memo || ""}
 						onChange={handleMemoChange}
 						className="w-full h-[311px] outline-none bg-transparent overflow-y:auto scrollbar-thin resize-none scrollbar-thin scrollbar-thumb-amber-200"
 					/>
 				</article>
 			</div>
 			<div className="flex gap-[7px] tablet:gap-[16px] desktop:justify-end justify-center mt-[24px]">
-				<button className="w-[168px] h-[56px] flex items-center justify-center  gap-[4px] rounded bg-primary-200">
+				<button
+					disabled={!isTodoDetailChanging}
+					className={`w-[168px] h-[56px] flex items-center ${
+						isTodoDetailChanging ? "bg-btn-complete" : "bg-primary-200"
+					} justify-center  gap-[4px] rounded `}
+				>
 					<Image alt="plus" src={check} />
 					<span>수정 완료</span>
 				</button>
-				<button className="w-[168px] text-small-semi text-white h-[56px] flex items-center justify-center  gap-[4px] rounded bg-btn-delete">
+				<button
+					onClick={onClickDeleteButton}
+					className="w-[168px] text-small-semi text-white h-[56px] flex items-center justify-center  gap-[4px] rounded bg-btn-delete"
+				>
 					<Image alt="plus" src={x} />
 					<span>삭제하기</span>
 				</button>
