62a793ed1ef20b6068c52a754c4045f1ab2c4624
dohyeons <ddhhss0603@gmail.com>
2024-12-31 19:11:25 +0900

    🔥 delete: 불필요한 설정 삭제

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -2,8 +2,7 @@ import { TodoListType } from "@/types";
 
 export default async function getItems(): Promise<TodoListType[]> {
 	const response = await fetch(
-		"https://assignment-todolist-api.vercel.app/api/ddhhss0603/items",
-		{ cache: "no-cache" }
+		"https://assignment-todolist-api.vercel.app/api/ddhhss0603/items"
 	).then(res => res.json());
 	return response;
 }
