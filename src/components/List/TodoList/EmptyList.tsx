309bb6ae6eabfd0ba07200e9350713d3710405ae
dohyeons <ddhhss0603@gmail.com>
2025-01-01 00:45:44 +0900

    💄 design: 비어있을 경우 글자 색 더 진하게

--- src/components/List/TodoList/EmptyList.tsx
+++ src/components/List/TodoList/EmptyList.tsx
@@ -9,7 +9,7 @@ export default function EmptyList() {
 				alt="todo_large"
 				className="size-[120px] tablet:size-[240px]"
 			/>
-			<span className="whitespace-pre-line text-center">
+			<span className="whitespace-pre-line text-center text-primary-400 text-small-semi">
 				할 일이 없어요.{"\n"}TODO를 새롭게 추가해주세요!
 			</span>
 		</div>
