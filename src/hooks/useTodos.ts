3ef140ebb25acd8432833a379fa12e1311a744e0
dohyeons <ddhhss0603@gmail.com>
2025-01-01 00:40:02 +0900

    ✨ feat: useTodos 업데이트

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -10,6 +10,7 @@ export default function useTodos() {
 	const [hasMore, setHasMore] = useState(true);
 	const { ref, inView } = useInView({ threshold: 0.5 });
 	const { ref: ref2, inView: inView2 } = useInView({ threshold: 0.5 });
+	const [isLoading2, setIsLoading2] = useState(false);
 
 	useEffect(() => {
 		if (page === 1) {
@@ -21,24 +22,32 @@ export default function useTodos() {
 				setIsLoading(false);
 			}
 			fetchInitialTodos();
-		} else if ((inView && hasMore) || (inView2 && hasMore)) {
+		}
+	}, []);
+	useEffect(() => {
+		console.log(hasMore, inView, page, todos, inView2, isLoading2);
+		if (
+			(page !== 1 && inView && hasMore && !isLoading2) ||
+			(page !== 1 && inView2 && hasMore && !isLoading2)
+		) {
 			async function fetchInitialTodos() {
+				setIsLoading2(() => true);
 				const additionalTodos = await getItems(page);
 				setTodos([...todos, ...additionalTodos]);
 				setHasMore(additionalTodos.length === 10);
 				setPage(page + 1);
+				setIsLoading2(() => false);
 			}
 			fetchInitialTodos();
 		}
-	}, [hasMore, inView, page, todos, inView2]);
+	}, [hasMore, inView, page, todos, inView2, isLoading2]);
 
 	const fetchTodos = async () => {
 		const data = await getItems(1);
-		setHasMore(true);
-		setPage(1);
+		setHasMore(data.length === 10);
+		setPage(2);
 		setTodos(data);
-		setIsLoading(false);
 	};
 
-	return { todos, isLoading, fetchTodos };
+	return { todos, isLoading, fetchTodos, ref, ref2 };
 }
