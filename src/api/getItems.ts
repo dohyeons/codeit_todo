f50f0ee661e29fe6ec6de7de9a8dfd57390a47af
dohyeons <ddhhss0603@gmail.com>
2024-12-31 16:38:42 +0900

    ✨ feat: cache설정 추가

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -2,7 +2,8 @@ import { TodoListType } from "@/types";
 
 export default async function getItems(): Promise<TodoListType[]> {
 	const response = await fetch(
-		"https://assignment-todolist-api.vercel.app/api/ddhhss0603/items"
+		"https://assignment-todolist-api.vercel.app/api/ddhhss0603/items",
+		{ cache: "no-cache" }
 	).then(res => res.json());
 	return response;
 }
