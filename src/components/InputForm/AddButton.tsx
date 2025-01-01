199b95ac2e308ff4e679c4927a1cafe49620bf5a
dohyeons <ddhhss0603@gmail.com>
2025-01-01 22:59:38 +0900

    ✨ feat: 신규 할 일 이름으로 공백 입력 불가능

--- src/components/InputForm/AddButton.tsx
+++ src/components/InputForm/AddButton.tsx
@@ -10,17 +10,18 @@ export default function AddButton({
 	onClick: () => void;
 	value: string;
 }) {
+	console.log(value.trim());
 	return (
 		<button
 			onClick={onClick}
-			disabled={value.length ? false : true}
+			disabled={value.trim() ? false : true}
 			className={`min-w-[56px] flex items-center justify-center shadow-inside  tablet:min-w-[168px] gap-[4px] rounded ${
-				value ? "bg-completed text-white" : "bg-primary-200"
+				value.trim() ? "bg-completed text-white" : "bg-primary-200"
 				// 값이 입력된 경우 버튼의 배경색이 흰색에서 보라색으로 변경
 			}`}
 		>
 			{/* 값이 입력된 경우 색이 다른 plus가 렌더링 */}
-			<Image alt="plus" src={value ? plus : plus_black} />
+			<Image alt="plus" src={value.trim() ? plus : plus_black} />
 			<span className="hidden tablet:block">추가하기</span>
 		</button>
 	);
