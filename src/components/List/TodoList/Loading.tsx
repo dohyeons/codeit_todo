4d472e795e92f6015fc171922ba674176b14bb37
dohyeons <ddhhss0603@gmail.com>
2025-01-01 22:28:24 +0900

    ♻️ refactor: 로딩 컴포넌트 텍스트 위치 변경

--- src/components/List/TodoList/Loading.tsx
+++ src/components/List/TodoList/Loading.tsx
@@ -1,10 +1,8 @@
 // todolist를 로딩중일 때 렌더링하는 컴포넌트
 export default function Loading() {
 	return (
-		<div className="w-full mt-[16px]">
-			<span className="text-primary-400 text-small-semi">
-				데이터를 불러오고 있습니다.
-			</span>
+		<div className="w-full text-center text-small-semi text-primary-400">
+			데이터를 불러오고있습니다.
 		</div>
 	);
 }
