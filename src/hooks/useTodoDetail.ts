2225d67be18a783abbbd142e464031d3634e5170
dohyeons <ddhhss0603@gmail.com>
2025-01-01 16:01:58 +0900

    ♻️ refactor: isChanged 함수 useEffect에서 분리

--- src/hooks/useTodoDetail.ts
+++ src/hooks/useTodoDetail.ts
@@ -11,17 +11,14 @@ export default function useTodoDetail(
 	});
 	const [isTodoDetailChanging, setIsTodoDetailChanging] = useState(false);
 
-	useEffect(() => {
-		function isChanged<T extends object>(obj1: T, obj2: T): boolean {
-			return (Object.keys(obj1) as (keyof T)[]).some(key => {
-				if (obj1[key] === null) {
-					return obj2[key] !== "";
-				}
-				return obj1[key] !== obj2[key];
-			});
-		}
-		setIsTodoDetailChanging(isChanged(initialDetail, todoDetail));
-	}, [initialDetail, todoDetail]);
+	function isChanged<T extends object>(obj1: T, obj2: T): boolean {
+		return (Object.keys(obj1) as (keyof T)[]).some(key => {
+			if (obj1[key] === null) {
+				return obj2[key] !== "";
+			}
+			return obj1[key] !== obj2[key];
+		});
+	}
 
 	function handleMemoChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
 		setTodoDetail({ ...todoDetail, memo: e.target.value });
@@ -36,6 +33,10 @@ export default function useTodoDetail(
 	function handleImageChange(uri: string) {
 		setTodoDetail({ ...todoDetail, imageUrl: uri });
 	}
+
+	useEffect(() => {
+		setIsTodoDetailChanging(isChanged(initialDetail, todoDetail));
+	}, [initialDetail, todoDetail]);
 
 	return {
 		todoDetail,
